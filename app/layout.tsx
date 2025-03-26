66b9e4f52a0e0099d758fb120dabfd7229dc0712
Elliot <etlin@ucdavis.edu>
2025-03-25 17:00:52 -0700

    removed redundant and quiz related files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,16 @@
+import "./globals.css";
+import { Metadata } from "next";
+import { Toaster } from "sonner";
 import { ThemeProvider } from "next-themes";
+import { Geist } from "next/font/google";
+
+const geist = Geist({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://ai-sdk-preview-pdf-support.vercel.app"),
+  title: "PDF Support Preview",
+  description: "Experimental preview of PDF support with the AI SDK",
+};
 
 export default function RootLayout({
   children,
@@ -6,12 +18,18 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className={`${geist.className}`}>
       <head>
         <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
       </head>
       <body>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider 
+          attribute="class" 
+          defaultTheme="dark"
+          enableSystem={false}
+          disableTransitionOnChange
+        >
+          <Toaster position="top-center" richColors />
           {children}
         </ThemeProvider>
       </body>
